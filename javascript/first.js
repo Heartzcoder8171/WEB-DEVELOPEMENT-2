033599876b7bec304ffb5a1205e62b7caac1ded7
Heartzcoder8171 <Dashingstar8171@gmail.com>
2024-10-23 11:35:49 +0530

    Add files via upload

---
+++ javascript/first.js
@@ -0,0 +1,31 @@
+// var a;
+// var a;
+// let b;
+// // const c;
+// const d = 1;
+// var a = 10;
+// a = 20;
+// let bb = 3;
+// bb = 6;
+// const s = 7;
+// {
+//     // let ab = 10;
+//     var  ab = 40;
+//     let cb = ab+20;
+
+// }
+// let ddd = ab+30;
+// console.log(ddd);
+// console.log(ddd,ab,bb);
+
+// Math object :
+
+let a = 10.5;
+console.log(Math.round(a));
+console.log(Math.floor(a));
+console.log(Math.ceil(a));
+console.log(Math.random());
+console.log(Math.random()*10);
+let randomVal = Math.random()*10;
+console.log(Math.round(randomVal));
+console.log(Math.round(Math.random()*(10-5))+5);
